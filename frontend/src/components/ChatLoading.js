2724b0703c8ef19fa3dd655eaeafadd5cd197fe7
MuditWadhwa <muditwadhwa1@gmail.com>
2022-01-09 16:13:51 +0530

    Final changes,deployment complete

--- frontend/src/components/ChatLoading.js
+++ frontend/src/components/ChatLoading.js
@@ -3,8 +3,7 @@ import React from 'react'
 
 const ChatLoading = () => {
     return (
-        <Stack>
-        
+        <Stack>        
             <Skeleton height='20px' />
   <Skeleton height='45px'/>
   <Skeleton height='45px'/>
